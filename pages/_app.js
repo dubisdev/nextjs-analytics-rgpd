59f2a8be9127e329efd2503f0e05af1dc67d2930
Deiv <77246331+Deivid-jmz@users.noreply.github.com>
2021-08-29 23:19:54 +0200

    Update Cookies Example Page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import CookieConsent from "react-cookie-consent";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+	const router = useRouter();
+
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+				page_path: url,
+			});
+		};
+		router.events.on("routeChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
+
+	return (
+		<>
+			<Component {...pageProps} />
+			<CookieConsent
+				location="bottom"
+				buttonText="Sí, utilizar cookies."
+				onAccept={() => location.reload()}
+				cookieName="CookieConsent"
+				expires={150}
+				enableDeclineButton="true"
+				declineButtonText="No, no utilizar cookies">
+				Poner aquí el mensaje sobre el uso de cookies
+				<a href="#enlace_hacia_politica_de_cookies">Política de Cookies</a>.
+			</CookieConsent>
+		</>
+	);
 }
 
-export default MyApp
+export default App;
